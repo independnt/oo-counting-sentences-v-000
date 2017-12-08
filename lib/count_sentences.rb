4d74712a1ede3b3296ad5953a5656d21e0c3a3c7
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-08 17:20:40 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else 
+      false
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else 
+      false
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else 
+      false
   end
 
   def count_sentences
-
+    self.split(".")
+    binding.pry
   end
 end
